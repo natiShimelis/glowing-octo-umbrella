4d698437a2fefb592565cb5e36aae748dd12ec58
NatiShimelis <natishimelisg@gmail.com>
2023-05-23 14:23:18 +0300

    added x-frame origin to same

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' ; style-s
 // Enable HSTS for one year (31536000 seconds) and include subdomains
 header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
 
+header("X-Frame-Options: SAMEORIGIN");
+
 ?>
 
 
