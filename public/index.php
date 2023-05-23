a70a173e240c203b56704ebd58747160c29dcca8
NatiShimelis <natishimelisg@gmail.com>
2023-05-23 14:36:33 +0300

    remove leakage of data through x-powered-by

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ header("X-Frame-Options: SAMEORIGIN");
 session_set_cookie_params(["SameSite" => "Strict"]); //none, lax, strict
 session_set_cookie_params(["Secure" => "true"]); //false, true
 session_set_cookie_params(["HttpOnly" => "true"]); //false, true
+header_remove('X-Powered-By');
 session_start(); //everything before this
 ?>
 
