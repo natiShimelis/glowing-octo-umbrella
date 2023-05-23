91204ba2341a589d6ee392740cd35378603b5592
NatiShimelis <natishimelisg@gmail.com>
2023-05-23 13:15:36 +0300

    add csp header for index route

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
+<?php
+header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
