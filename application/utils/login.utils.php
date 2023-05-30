f8aec021a8ee300770c89c437955001c38fcc4ee
NatiShimelis <natishimelisg@gmail.com>
2023-05-30 13:41:10 +0300

    code to be sql injection vulnerable

--- application/utils/login.utils.php
+++ application/utils/login.utils.php
@@ -6,17 +6,9 @@
     }
 
     function checkFileExists($conn, $email) {
-        $sql = "SELECT * FROM User WHERE `Email` = ?;";
-        
-        $stmt = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            return false;
-        }
+        $sql = "SELECT * FROM User WHERE `Email` = '$email'";
 
-        mysqli_stmt_bind_param($stmt, "s" , $email);
-        mysqli_stmt_execute($stmt);
-
-        $result = mysqli_stmt_get_result($stmt);
+        $result = mysqli_query($conn,$sql);
 
         if($row = mysqli_fetch_assoc($result)) {
             return $row;
